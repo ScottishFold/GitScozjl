1af6e8b79feb02ff3174a387061be23086130a3e
ScoZjl <scozjl@scozjl.local>
2016-11-09 11:11:43 +0800

    Initial Commit

---
+++ GitUseage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseage
+//
+//  Created by scozjl on 16/11/9.
+//  Copyright © 2016年 Asiasys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitUseageTests/GitUseageTests.m
@@ -0,0 +1,39 @@
+//
+//  GitUseageTests.m
+//  GitUseageTests
+//
+//  Created by scozjl on 16/11/9.
+//  Copyright © 2016年 Asiasys. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitUseageTests : XCTestCase
+
+@end
+
+@implementation GitUseageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
